7b0cda58a60cb4504d61a6fa159392b931c5a03d
Siddharth Vats <siddharth.abode@gmail.com>
2025-03-22 13:05:52 +0530

    Highlighting line number in neovim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,3 +4,8 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
 vim.opt.number = true
+vim.cmd([[
+set cursorline
+set cursorlineopt=number
+autocmd ColorScheme * highlight CursorLineNr cterm=bold term=bold gui=bold
+]])                -- Highlights line number
